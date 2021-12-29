5cdf6db69d3d980a06634517b46e7f6d3246e121
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2021-12-29 20:14:16 +0330

    Conversion countdown func and decrease func

--- ex1/ex1.js
+++ ex1/ex1.js
@@ -1,11 +1,13 @@
-const splitBill = (amount , numPeople) => (`Each person needs to pay ${amount / numPeople}`)
+const splitBill = (amount , numPeople) => `Each person needs to pay ${amount / numPeople}`;
 
+console.log(splitBill(10,2));
+console.log(splitBill(10,4));
 
-function countdown(startingNumber ,step){
-    let countFromNum = startingNumber + step
-    return function decrease(){
-    countFromNum -=step;
-    return countFromNum;
-    }
+const countdown = (startingNumber ,step) => {
+    let countFromNum = startingNumber + step;
+    const decrease = () => countFromNum -=step;
+    return decrease;
 }
-const countingDown =countdown(20,2);
+
+const countingDown = countdown(20,2);
+console.log(countingDown());

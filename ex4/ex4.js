f33f51c7c18eebb3a333fd18136bd188e37a7bfa
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2021-12-29 21:50:15 +0330

    console log for 3 input

--- ex4/ex4.js
+++ ex4/ex4.js
@@ -1,5 +1,17 @@
 const ingExtractor = (input) => {
-
+    return input.replace(/[^a-zA-Z ]/g, "")
+    .split(" ")
+    .filter((word) => {
+        let char = word.toLowerCase().replace("ing", "");
+        if (char.length === char.replace(/[aeiou]/g, "").length) {
+            return false;
+        }
+        if (word.toLowerCase().includes("ing")) {
+            return true;
+        }
+    });
 }
 
 console.log(ingExtractor("coming bringing Letting sing"));
+console.log(ingExtractor("going Ping, king sHrink dOing"));
+console.log(ingExtractor("zing went ring, ding wing SINk"));
